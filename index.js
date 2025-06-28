d8b6d7bdd9a598e81557f253fc05423e61c62f3a
michael <ei.dwaps@gmail.com>
2025-06-28 18:41:50 -0400

    add features for format text

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const quill = new Quill("#editor-container", {
   theme: "snow",
-  placeholder: "Écris ici avec la police ColeCarreira...",
+  placeholder: "Écris ce que tu veux 😘",
   modules: {
     toolbar: [
       [{ header: [1, 2, false] }],
       ["bold", "italic", "underline"],
       ["link", "image"],
       [{ list: "ordered" }, { list: "bullet" }],
+      [{ align: [] }],
+      [{ color: [] }],
       ["clean"],
     ],
   },
